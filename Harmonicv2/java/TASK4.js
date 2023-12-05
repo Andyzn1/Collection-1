a8433cf4a547d193d735546a8f3cba05ea2caccd
Andyzn1 <143567401+Andyzn1@users.noreply.github.com>
2023-12-05 16:07:10 -0500

    Delete Harmonicv2 directory

--- Harmonicv2/java/TASK4.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.element-r').addEventListener('click', function () {
-    var randomSize = Math.random() * (1.5 - 0.8) + 0.8; // Adjust the range as needed
-    this.style.transform = 'scale(' + randomSize + ')';
-});
